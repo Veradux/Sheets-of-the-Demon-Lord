d95f43dd4a8bed2c8bd2aafabefd618e317e2053
Veradux <veradux@abv.bg>
2023-04-07 13:02:11 +0300

    Add floating action button for adding new character sheets

--- app/src/main/java/com/veradux/sheetsofthedemonlord/MainActivity.kt
+++ app/src/main/java/com/veradux/sheetsofthedemonlord/MainActivity.kt
@@ -3,8 +3,14 @@ package com.veradux.sheetsofthedemonlord
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.veradux.sheetsofthedemonlord.characters.data.mock.CharactersApiMock
@@ -18,10 +24,24 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             SheetsOfTheDemonLordTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    // TODO figure out how to use view models and APIs
-                    val api = CharactersApiMock()
-                    CharacterList(characters = api.getCharacters())
+                Scaffold(
+                    floatingActionButtonPosition = FabPosition.End,
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = { /* OnClick Method */ },
+                        ) {
+                            Icon(
+                                imageVector = Icons.Rounded.Add,
+                                contentDescription = "Add Character Sheet",
+                            )
+                        }
+                    }
+                ) {
+                    Surface(modifier = Modifier.padding(it), color = MaterialTheme.colorScheme.background) {
+                        // TODO figure out how to use view models and APIs
+                        val api = CharactersApiMock()
+                        CharacterList(characters = api.getCharacters())
+                    }
                 }
             }
         }
