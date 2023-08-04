a49dede71326aec77749857dbf10a9cd9583811f
Veradux <veradux@abv.bg>
2023-08-04 08:55:43 +0300

    Fix all build errors and move the navgraph to its own file

---
+++ app/src/main/java/com/veradux/sheetsofthedemonlord/characters/CharactersScreen.kt
@@ -0,0 +1,8 @@
+package com.veradux.sheetsofthedemonlord.characters
+
+object CharactersScreen {
+    const val ROUTE = "CharactersScreenROUTE"
+    const val LIST = "CharactersScreenLIST"
+    const val SHEET = "CharactersScreenSHEET"
+    const val CREATION = "CharactersScreenCREATION"
+}
