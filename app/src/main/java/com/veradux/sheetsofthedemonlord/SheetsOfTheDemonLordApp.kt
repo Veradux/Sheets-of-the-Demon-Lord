b4abd179a8201d8c6d4c8acbf816cebfc5f95b92
Veradux <veradux@abv.bg>
2023-08-09 14:10:44 +0300

    Add a snackbar for the buttons which are for unimplemented screens; Add the app title at the top of the drawer

--- app/src/main/java/com/veradux/sheetsofthedemonlord/SheetsOfTheDemonLordApp.kt
+++ app/src/main/java/com/veradux/sheetsofthedemonlord/SheetsOfTheDemonLordApp.kt
@@ -4,10 +4,11 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -15,14 +16,20 @@ import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
@@ -32,15 +39,15 @@ import kotlinx.coroutines.launch
 
 @Composable
 fun SheetsOfTheDemonLordApp() {
-    Scaffold { paddingValues ->
+    val snackBarHostState = remember { SnackbarHostState() }
+
+    Scaffold(snackbarHost = { SnackbarHost(snackBarHostState) }) { paddingValues ->
         Surface(modifier = Modifier.padding(paddingValues), color = MaterialTheme.colorScheme.background) {
             val navController = rememberNavController()
             val drawerViewModel = NavDrawerViewModel()
-            DemonLordNavigationDrawer(navController, drawerViewModel) {
-                NavHost(
-                    navController = navController,
-                    startDestination = CharactersScreen.ROUTE,
-                ) {
+            DemonLordNavigationDrawer(navController, snackBarHostState, drawerViewModel) {
+                NavHost(navController = navController, startDestination = CharactersScreen.ROUTE) {
+
                     charactersNavGraph(navController, drawerViewModel::updateSelectedButtonState)
                     // add other graphs for the other screens here
                 }
@@ -52,6 +59,7 @@ fun SheetsOfTheDemonLordApp() {
 @Composable
 fun DemonLordNavigationDrawer(
     navController: NavHostController,
+    snackBarHostState: SnackbarHostState,
     viewModel: NavDrawerViewModel,
     content: @Composable () -> Unit
 ) {
@@ -63,33 +71,57 @@ fun DemonLordNavigationDrawer(
         content = content,
         drawerContent = {
             ModalDrawerSheet {
+                Text(
+                    text = stringResource(R.string.app_name).uppercase(),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 20.sp,
+                    color = MaterialTheme.colorScheme.primary,
+                    modifier = Modifier.padding(32.dp)
+                )
                 getDemonLordNavButtons().forEach { button ->
-                    NavigationDrawerItem(
-                        label = { Text(text = stringResource(button.label)) },
-                        icon = { Icon(button.icon, stringResource(button.label)) },
-                        selected = viewModel.isButtonSelected(button.screenRoute),
-                        onClick = {
-                            viewModel.updateSelectedButtonState(button.screenRoute)
-                            scope.launch { drawerState.close() }
-                            navController.navigate(button.screenRoute)
-                        }
-                    )
+                    button?.let {
+                        NavigationDrawerItem(
+                            modifier = Modifier.padding(horizontal = 16.dp),
+                            label = { Text(text = stringResource(button.label)) },
+                            icon = { Icon(button.icon, stringResource(button.label)) },
+                            selected = viewModel.isButtonSelected(button.screenRoute ?: ""),
+                            onClick = {
+                                if (button.screenRoute != null) {
+                                    scope.launch { drawerState.close() }
+                                    navController.navigate(button.screenRoute)
+                                    viewModel.updateSelectedButtonState(button.screenRoute)
+                                } else {
+                                    scope.launch {
+                                        snackBarHostState.showSnackbar("This feature will be implemented soon!")
+                                    }
+                                }
+                            }
+                        )
+                    }
+                    if (button == null) Divider(modifier = Modifier.padding(horizontal = 32.dp, vertical = 8.dp))
                 }
             }
         }
     )
 }
 
-private fun getDemonLordNavButtons(): List<NavButton> = listOf(
-    NavButton(R.string.new_character, CharactersScreen.CREATION, Icons.Default.Add),
+/**
+ * The order of the returned list determines the order of the UI buttons in the drawer.
+ * The list has nullable objects and the nulls are used to signify positions for dividers in the drawer.
+ */
+private fun getDemonLordNavButtons(): List<NavButton?> = listOf(
     NavButton(R.string.characters, CharactersScreen.LIST, Icons.Default.List),
-    NavButton(R.string.spells, CharactersScreen.CREATION, Icons.Default.Info),
-    NavButton(R.string.ancestries, CharactersScreen.CREATION, Icons.Default.Face),
-    NavButton(R.string.combat_actions, CharactersScreen.CREATION, Icons.Default.ArrowDropDown)
+    NavButton(R.string.new_character, CharactersScreen.CREATION, Icons.Default.Add),
+    null,
+    NavButton(R.string.spells, null, Icons.Default.Info),
+    NavButton(R.string.ancestries, null, Icons.Default.Face),
+    NavButton(R.string.combat_actions, null, Icons.Default.KeyboardArrowDown)
 )
 
 private data class NavButton(
     @StringRes val label: Int,
-    val screenRoute: String,
+    // TODO the nullable is added temporarily to be used for nav buttons which do not lead to a screen yet.
+    //  When all screens are developed, the nullable can be removed.
+    val screenRoute: String?,
     val icon: ImageVector
 )
