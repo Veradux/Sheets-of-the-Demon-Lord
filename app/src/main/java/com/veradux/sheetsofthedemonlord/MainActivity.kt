27a2a0c8523cb4081eeae79a86d40888935c3808
Veradux <veradux@abv.bg>
2023-08-09 20:13:55 +0300

    Add basic firebase setup

--- app/src/main/java/com/veradux/sheetsofthedemonlord/MainActivity.kt
+++ app/src/main/java/com/veradux/sheetsofthedemonlord/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
 import com.veradux.sheetsofthedemonlord.gameinfo.parsePdfSpells
 import com.veradux.sheetsofthedemonlord.ui.theme.SheetsOfTheDemonLordTheme
 
 class MainActivity : ComponentActivity() {
 
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (!BuildConfig.DEBUG) {
+            firebaseAnalytics = Firebase.analytics
+        }
 
         val spells = parsePdfSpells(assets.open("text/some spells.txt"))
         Log.d("pdf", Gson().toJson(spells))
