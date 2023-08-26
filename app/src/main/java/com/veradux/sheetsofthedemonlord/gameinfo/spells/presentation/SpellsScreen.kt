3382c2a1c8b041476796952fcd7fa998a848c6d4
Veradux <veradux@abv.bg>
2023-08-26 20:26:27 +0300

    Hide search bar when scrolling down

--- app/src/main/java/com/veradux/sheetsofthedemonlord/gameinfo/spells/presentation/SpellsScreen.kt
+++ app/src/main/java/com/veradux/sheetsofthedemonlord/gameinfo/spells/presentation/SpellsScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -40,6 +41,7 @@ import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.veradux.sheetsofthedemonlord.R
 import com.veradux.sheetsofthedemonlord.gameinfo.spells.model.SpellFilterCategories
+import com.veradux.sheetsofthedemonlord.ui.composables.ScreenWithScrollableTopBar
 
 @Composable
 fun SpellsScreen(viewModel: SpellsScreenViewModel = viewModel()) {
@@ -48,88 +50,102 @@ fun SpellsScreen(viewModel: SpellsScreenViewModel = viewModel()) {
     val spellFilters by viewModel.spellFilters.collectAsState()
     var isActive by rememberSaveable { mutableStateOf(false) }
 
-    Column {
-        SpellsSearchBar(
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            query = viewModel.searchBarText.collectAsState().value,
-            onQueryChange = viewModel::onSearchBarTextChange,
-            isActive = isActive,
-            setActiveStateTo = { isActive = it },
-            setFilterDialogVisibilityStateTo = viewModel::setFilterDialogVisibilityStateTo
-        ) {
-            Text(
-                text = stringResource(R.string.x_spells_found, filteredSpells.count()),
+    ScreenWithScrollableTopBar(
+        topBarHeight = 64.dp,
+        topBar = { offset ->
+            SpellsSearchBar(
                 modifier = Modifier
-                    .padding(horizontal = 24.dp)
-                    .align(Alignment.End)
-            )
-            LazyColumn {
-                items(filteredSpells) { spell ->
-                    SpellTitle(spell, modifier = Modifier.padding(horizontal = 24.dp, vertical = 12.dp))
+                    .align(Alignment.TopCenter)
+                    .offset { offset },
+                query = viewModel.searchBarText.collectAsState().value,
+                onQueryChange = viewModel::onSearchBarTextChange,
+                isActive = isActive,
+                setActiveStateTo = { isActive = it },
+                setFilterDialogVisibilityStateTo = viewModel::setFilterDialogVisibilityStateTo
+            ) {
+                Text(
+                    text = stringResource(R.string.x_spells_found, filteredSpells.count()),
+                    modifier = Modifier
+                        .padding(horizontal = 24.dp)
+                        .align(Alignment.End)
+                )
+                LazyColumn {
+                    items(filteredSpells) { spell ->
+                        SpellTitle(spell, modifier = Modifier.padding(horizontal = 24.dp, vertical = 12.dp))
+                    }
                 }
             }
         }
-
+    ) { paddingValues ->
         AnimatedVisibility(filteredSpells.isEmpty()) {
             Text(
                 text = stringResource(R.string.no_spells_found),
                 textAlign = TextAlign.Center,
                 modifier = Modifier
-                    .padding(32.dp)
-                    .align(Alignment.CenterHorizontally)
+                    .padding(horizontal = 32.dp, vertical = 120.dp)
+                    .align(Alignment.Center)
             )
         }
+        // TODO fix enter transition
         AnimatedVisibility(filteredSpells.isNotEmpty()) {
-            LazyColumn {
+            LazyColumn(contentPadding = paddingValues) {
                 items(filteredSpells) { Spell(it) }
             }
         }
-    }
 
-    if (isFilterDialogOpen) {
-        Dialog(
-            onDismissRequest = { viewModel.setFilterDialogVisibilityStateTo(false) },
-            properties = DialogProperties(usePlatformDefaultWidth = false)
-        ) {
-            Surface {
-                Column(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxSize()
-                        .verticalScroll(rememberScrollState())
-                ) {
-                    IconButton(
-                        modifier = Modifier.align(Alignment.End),
-                        onClick = { viewModel.setFilterDialogVisibilityStateTo(false) }) {
-                        Icon(Icons.Filled.Menu, contentDescription = "Filter spells")
-                    }
+        if (isFilterDialogOpen) {
+            Dialog(
+                onDismissRequest = { viewModel.setFilterDialogVisibilityStateTo(false) },
+                properties = DialogProperties(usePlatformDefaultWidth = false)
+            ) {
+                Surface {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxSize()
+                            .verticalScroll(rememberScrollState())
+                    ) {
+                        IconButton(
+                            modifier = Modifier.align(Alignment.End),
+                            onClick = { viewModel.setFilterDialogVisibilityStateTo(false) }) {
+                            Icon(Icons.Filled.Menu, contentDescription = "Filter spells")
+                        }
 
-                    FilterCategory(R.string.traditions, Icons.Default.Star, spellFilters.traditionFilters) { toggle ->
-                        viewModel.updateSpellFilters(spellFilters.traditionFilters, toggle) {
-                            spellFilters.copy(traditionFilters = it)
+                        FilterCategory(
+                            R.string.traditions,
+                            Icons.Default.Star,
+                            spellFilters.traditionFilters
+                        ) { toggle ->
+                            viewModel.updateSpellFilters(spellFilters.traditionFilters, toggle) {
+                                spellFilters.copy(traditionFilters = it)
+                            }
                         }
-                    }
-                    Divider(modifier = Modifier.padding(8.dp))
-                    FilterCategory(R.string.level, Icons.Default.Star, spellFilters.levelFilters) { toggle ->
-                        viewModel.updateSpellFilters(spellFilters.levelFilters, toggle) {
-                            spellFilters.copy(levelFilters = it)
+                        Divider(modifier = Modifier.padding(8.dp))
+                        FilterCategory(R.string.level, Icons.Default.Star, spellFilters.levelFilters) { toggle ->
+                            viewModel.updateSpellFilters(spellFilters.levelFilters, toggle) {
+                                spellFilters.copy(levelFilters = it)
+                            }
                         }
-                    }
 
-                    Divider(modifier = Modifier.padding(8.dp))
-                    FilterCategory(R.string.properties, Icons.Default.Star, spellFilters.propertyFilters) { toggle ->
-                        viewModel.updateSpellFilters(spellFilters.propertyFilters, toggle) {
-                            spellFilters.copy(propertyFilters = it)
+                        Divider(modifier = Modifier.padding(8.dp))
+                        FilterCategory(
+                            R.string.properties,
+                            Icons.Default.Star,
+                            spellFilters.propertyFilters
+                        ) { toggle ->
+                            viewModel.updateSpellFilters(spellFilters.propertyFilters, toggle) {
+                                spellFilters.copy(propertyFilters = it)
+                            }
                         }
-                    }
-                    Divider(modifier = Modifier.padding(8.dp))
-                    FilterCategory(
-                        R.string.source_book,
-                        Icons.Default.Star,
-                        spellFilters.sourceBookFilters
-                    ) { toggle ->
-                        viewModel.updateSpellFilters(spellFilters.sourceBookFilters, toggle) {
-                            spellFilters.copy(sourceBookFilters = it)
+                        Divider(modifier = Modifier.padding(8.dp))
+                        FilterCategory(
+                            R.string.source_book,
+                            Icons.Default.Star,
+                            spellFilters.sourceBookFilters
+                        ) { toggle ->
+                            viewModel.updateSpellFilters(spellFilters.sourceBookFilters, toggle) {
+                                spellFilters.copy(sourceBookFilters = it)
+                            }
                         }
                     }
                 }
