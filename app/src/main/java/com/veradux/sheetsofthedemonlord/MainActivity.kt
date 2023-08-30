e8b9a64c6edb5b5cd374378f2e6033caca34d963
Veradux <veradux@abv.bg>
2023-08-30 21:46:26 +0300

    Merge with develop

--- app/src/main/java/com/veradux/sheetsofthedemonlord/MainActivity.kt
+++ app/src/main/java/com/veradux/sheetsofthedemonlord/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.veradux.sheetsofthedemonlord
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.analytics
 import com.google.firebase.ktx.Firebase
-import com.google.gson.Gson
 import com.veradux.sheetsofthedemonlord.gameinfo.parsePdfSpells
 import com.veradux.sheetsofthedemonlord.gameinfo.spells.model.Spell
 import com.veradux.sheetsofthedemonlord.ui.theme.SheetsOfTheDemonLordTheme
